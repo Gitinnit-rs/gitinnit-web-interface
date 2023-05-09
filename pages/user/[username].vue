3ee1277d247c899a39e4717cf9ac9b4ace2235ff
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2023-05-09 12:10:39 +0530

    ft: music id page, fix link to username page

--- pages/user/[username].vue
+++ pages/user/[username].vue
@@ -7,73 +7,81 @@ const route = useRoute();
 const username = route.params.username;
 
 const {
-  data: user,
-  pending,
-  error,
+    data: user,
+    pending,
+    error,
 } = useFetch<any[]>(BASE_URL + "/user?includeMusic=true&username=" + username);
 
-const music = computed(() => (user.value as any[])[0].music as Music[]);
+const music = computed(
+    () => user.value && ((user.value as any[])[0].music as Music[])
+);
 
 useHead({
-  title: user.value ? user.value[0].name : "User",
+    title: user.value ? user.value[0].name : "User",
 });
 
 console.log({ music, user });
 </script>
 <template>
-  <section v-if="!username" class="p-10">
-    <h1>Profile {{ username }} not found</h1>
-  </section>
-  <section v-else-if="pending && !user && !music">Loading...</section>
-  <section v-else-if="error && !user && !music" class="p-10">
-    <div class="alert alert-error shadow-lg">
-      <div>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="stroke-current flex-shrink-0 h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
-          />
-        </svg>
-        <span>
-          An error occurred while fetching user details<br />{{ error }}
-        </span>
-      </div>
-    </div>
-  </section>
-  <section v-else-if="user">
-    <div class="hero py-10 bg-base-200">
-      <div class="hero-content text-center">
-        <div class="max-w-md">
-          <img
-            :src="user[0].profile_photo"
-            class="w-[20rem] h-[20rem] rounded-lg shadow-2xl object-cover"
-            alt="Gitinnit User's Profile Image"
-          />
-          <div class="mt-6">
-            <h1 class="text-4xl font-bold">{{ user[0].name }}</h1>
-            <p class="mt-1">{{ user[0].bio }}</p>
-          </div>
+    <section v-if="!username" class="p-10">
+        <h1>Profile {{ username }} not found</h1>
+    </section>
+    <section v-else-if="pending && !user && !music">Loading...</section>
+    <section v-else-if="error && !user && !music" class="p-10">
+        <div class="alert alert-error shadow-lg">
+            <div>
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="stroke-current flex-shrink-0 h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                >
+                    <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+                    />
+                </svg>
+                <span>
+                    An error occurred while fetching user details<br />{{
+                        error
+                    }}
+                </span>
+            </div>
         </div>
-      </div>
-    </div>
+    </section>
+    <section v-else-if="user">
+        <div class="hero py-10 bg-base-200">
+            <div class="hero-content text-center">
+                <div class="max-w-md">
+                    <img
+                        :src="user[0].profile_photo"
+                        class="w-[20rem] h-[20rem] rounded-lg shadow-2xl object-cover"
+                        alt="Gitinnit User's Profile Image"
+                    />
+                    <div class="mt-6">
+                        <h1 class="text-4xl font-bold">{{ user[0].name }}</h1>
+                        <p class="mt-1">{{ user[0].bio }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
 
-    <div class="p-10">
-      <h1 class="text-4xl font-bold">Music</h1>
+        <div class="p-10">
+            <h1 class="text-4xl font-bold">Music</h1>
 
-      <!-- List of Music Items -->
-      <div class="mt-6 w-1/2">
-        <p v-if="!music || music.length === 0">No music yet.</p>
-        <template v-else>
-          <MusicItem v-for="item in music" :key="item.id" :music="item" />
-        </template>
-      </div>
-    </div>
-  </section>
+            <!-- List of Music Items -->
+            <div class="mt-6 w-1/2">
+                <p v-if="!music || music.length === 0">No music yet.</p>
+                <template v-else>
+                    <MusicItem
+                        v-for="item in music"
+                        :key="item.id"
+                        :music="item"
+                    />
+                </template>
+            </div>
+        </div>
+    </section>
 </template>
--- types/index.ts
+++ types/index.ts
@@ -1,18 +1,19 @@
 export interface Music {
-  id: string;
-  name: string;
-  release_date: string;
-  tags: string[];
-  genre: string[];
-  like_count: number;
-  music_url: string;
-  cover_url: string;
-  artist_id: string;
+    id: string;
+    name: string;
+    release_date: string;
+    tags: string[];
+    genre: string[];
+    like_count: number;
+    music_url: string;
+    cover_url: string;
+    artist_id: string;
 
-  artists: Artist[];
+    artists: Artist[];
 }
 
 export interface Artist {
-  name: string;
-  id: string;
+    id: string;
+    name: string;
+    username: string;
 }
